7d8f18b401f447268e34c6bca9c4a8cb72495c70
Harshit Gajjar <hrstgajjar3@gmail.com>
2020-06-04 16:43:07 +0400

    Initial Commit

---
+++ AvacadoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AvacadoApp
+//
+//  Created by Harshit Gajjar on 6/4/20.
+//  Copyright © 2020 ThinkX. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
